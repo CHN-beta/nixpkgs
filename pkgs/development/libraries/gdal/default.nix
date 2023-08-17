d18ed60d1565724e21823babc2e13b11148e78ea
chn <chn@chn.moe>
2023-08-17 20:33:56 +0800

    disable failed test for znver3

--- pkgs/development/libraries/gdal/default.nix
+++ pkgs/development/libraries/gdal/default.nix
@@ -210,7 +210,7 @@ stdenv.mkDerivation (finalAttrs: {
     "test_rda_download_queue"
   ] ++ lib.optionals (lib.versionOlder proj.version "8") [
     "test_ogr_parquet_write_crs_without_id_in_datum_ensemble_members"
-  ] ++ lib.optionals ( builtins.elem stdenv.hostPlatform.gcc.arch or "" [ "alderlake" "znver3" ] ) [
+  ] ++ lib.optionals (builtins.elem stdenv.hostPlatform.gcc.arch or "" [ "alderlake" "znver3" ]) [
     "test_jp2openjpeg_22"
   ];
   postCheck = ''
