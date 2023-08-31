a832e7138776d1579e60be976d4b0c3101a2eed0
chn <chn@chn.moe>
2023-08-31 22:08:35 +0800

    add intel mpi

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21948,6 +21948,8 @@ with pkgs;
 
   intel-media-driver = callPackage ../development/libraries/intel-media-driver { };
 
+  intel-mpi = callPackage ../development/libraries/intel-mpi { };
+
   intltool = callPackage ../development/tools/misc/intltool { };
 
   ios-cross-compile = callPackage ../development/compilers/ios-cross-compile/9.2.nix { };
