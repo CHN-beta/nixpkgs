e1f5d687fe67bd7b892ffa66f482d17673ee91a3
chn <chn@chn.moe>
2023-12-04 10:43:24 +0800

    ccache: ignore time_macros

--- nixos/modules/programs/ccache.nix
+++ nixos/modules/programs/ccache.nix
@@ -63,7 +63,7 @@ in {
               export CCACHE_DIR="${cfg.cacheDir}"
               export CCACHE_UMASK=007
               export CCACHE_MAXSIZE=300G
-              CCACHE_SLOPPINESS=random_seed
+              CCACHE_SLOPPINESS=random_seed,time_macros
               if [ ! -d "$CCACHE_DIR" ]; then
                 echo "====="
                 echo "Directory '$CCACHE_DIR' does not exist"
