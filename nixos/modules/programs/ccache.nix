3517378072c4328ff254dd7f7fa334736e5f7a21
chn <chn@chn.moe>
2024-07-09 17:29:23 +0800

    set ccache size

--- nixos/modules/programs/ccache.nix
+++ nixos/modules/programs/ccache.nix
@@ -70,7 +70,7 @@ in {
               export CCACHE_NOCOMPRESS=true
               export CCACHE_DIR="${cfg.cacheDir}"
               export CCACHE_UMASK=007
-              export CCACHE_MAXSIZE=300G
+              export CCACHE_MAXSIZE=100G
               export CCACHE_SLOPPINESS=random_seed,time_macros
               export CCACHE_NOHASHDIR=true
               export CCACHE_BASEDIR="$NIX_BUILD_TOP"
