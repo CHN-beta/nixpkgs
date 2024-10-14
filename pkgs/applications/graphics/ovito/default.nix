a60d02ef57437f359be6c57d5247c9eb96174345
chn <chn@chn.moe>
2024-10-14 15:26:34 +0800

    ovito: 3.7.11 -> 3.11.0

--- pkgs/applications/graphics/ovito/default.nix
+++ pkgs/applications/graphics/ovito/default.nix
@@ -1,7 +1,8 @@
-{ mkDerivation
-, lib
+{ lib
 , stdenv
 , fetchFromGitLab
+, fetchurl
+, makeDesktopItem
 , cmake
 , boost
 , bzip2
@@ -17,21 +18,25 @@
 , qtsvg
 , qttools
 , VideoDecodeAcceleration
+, wrapQtAppsHook
+# needed to run natively on wayland
+, qtwayland
 }:
 
-mkDerivation rec {
+stdenv.mkDerivation rec {
   pname = "ovito";
-  version = "3.7.11";
+  version = "3.11.0";
 
   src = fetchFromGitLab {
     owner = "stuko";
     repo = "ovito";
     rev = "v${version}";
-    hash = "sha256-Z3uwjOYJ7di/LLllbzdKjzUE7m119i03bA8dJPqhxWA=";
+    hash = "sha256-egiA6z1e8ZS7i4CIVjsCKJP1wQSRpmSKitoVTszu0Mc=";
   };
 
   nativeBuildInputs = [
     cmake
+    wrapQtAppsHook
   ];
 
   buildInputs = [
@@ -48,16 +53,43 @@ mkDerivation rec {
     qtbase
     qtsvg
     qttools
+    qtwayland
   ] ++ lib.optionals stdenv.hostPlatform.isDarwin [
     VideoDecodeAcceleration
   ];
+
+  # manually create a desktop file
+  postInstall =
+    let
+      icon = fetchurl {
+        url = "https://www.ovito.org/wp-content/uploads/logo_rgb-768x737.png";
+        sha256 = "1wp9c61cdw1bis4ykn8sdi3mr2ah6jl3bl5bil6fx6yywm8qis8l";
+      };
+      desktopFile = makeDesktopItem {
+        name = pname;
+        comment= "Open Visualization Tool";
+        exec = "@out@/bin/ovito";
+        inherit icon;
+        terminal = false;
+        startupNotify = false;
+        desktopName = "ovito";
+        startupWMClass = "Ovito";
+        categories = [ "Science" ];
+      };
+    in ''
+      mkdir -p $out/share/applications
+      substituteAll ${desktopFile}/share/applications/${pname}.desktop $out/share/applications/${pname}.desktop
+    '';
 
   meta = with lib; {
     description = "Scientific visualization and analysis software for atomistic and particle simulation data";
     mainProgram = "ovito";
     homepage = "https://ovito.org";
     license = with licenses;  [ gpl3Only mit ];
-    maintainers = with maintainers; [ twhitehead ];
+    maintainers = with maintainers; [
+      twhitehead
+      chn
+    ];
     broken = stdenv.hostPlatform.isDarwin; # clang-11: error: no such file or directory: '$-DOVITO_COPYRIGHT_NOTICE=...
   };
 }
