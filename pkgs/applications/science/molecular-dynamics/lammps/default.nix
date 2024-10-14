be59b939f48beb289451f1623e905419684976a2
chn <chn@chn.moe>
2024-10-14 15:59:59 +0800

    update lammps

--- pkgs/applications/science/molecular-dynamics/lammps/default.nix
+++ pkgs/applications/science/molecular-dynamics/lammps/default.nix
@@ -1,59 +1,62 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, libpng
-, gzip
-, fftw
-, blas
-, lapack
-, cmake
-, autoAddDriverRunpath
-, pkg-config
-# Available list of packages can be found near here:
-#
-# - https://github.com/lammps/lammps/blob/develop/cmake/CMakeLists.txt#L222
-# - https://docs.lammps.org/Build_extras.html
-, packages ? {
-  ASPHERE = true;
-  BODY = true;
-  CLASS2 = true;
-  COLLOID = true;
-  COMPRESS = true;
-  CORESHELL = true;
-  DIPOLE = true;
-  GRANULAR = true;
-  KSPACE = true;
-  MANYBODY = true;
-  MC = true;
-  MISC = true;
-  MOLECULE = true;
-  OPT = true;
-  PERI = true;
-  QEQ = true;
-  REPLICA = true;
-  RIGID = true;
-  SHOCK = true;
-  ML-SNAP = true;
-  SRD = true;
-  REAXFF = true;
-}
-# Extra cmakeFlags to add as "-D${attr}=${value}"
-, extraCmakeFlags ? {}
-# Extra `buildInputs` - meant for packages that require more inputs
-, extraBuildInputs ? []
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  libpng,
+  gzip,
+  fftw,
+  blas,
+  lapack,
+  python3,
+  cmake,
+  autoAddDriverRunpath,
+  pkg-config,
+  # Available list of packages can be found near here:
+  #
+  # - https://github.com/lammps/lammps/blob/develop/cmake/CMakeLists.txt#L222
+  # - https://docs.lammps.org/Build_extras.html
+  packages ? {
+    ASPHERE = true;
+    BODY = true;
+    CLASS2 = true;
+    COLLOID = true;
+    COMPRESS = true;
+    CORESHELL = true;
+    DIPOLE = true;
+    GRANULAR = true;
+    KSPACE = true;
+    MANYBODY = true;
+    MC = true;
+    MISC = true;
+    MOLECULE = true;
+    OPT = true;
+    PERI = true;
+    QEQ = true;
+    REPLICA = true;
+    RIGID = true;
+    SHOCK = true;
+    ML-SNAP = true;
+    SRD = true;
+    REAXFF = true;
+    PYTHON = true;
+  },
+  # Extra cmakeFlags to add as "-D${attr}=${value}"
+  extraCmakeFlags ? { },
+  # Extra `buildInputs` - meant for packages that require more inputs
+  extraBuildInputs ? [ ],
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   # LAMMPS has weird versioning convention. Updates should go smoothly with:
   # nix-update --commit lammps --version-regex 'stable_(.*)'
-  version = "2Aug2023_update3";
+  version = "29Aug2024_update1";
   pname = "lammps";
 
   src = fetchFromGitHub {
     owner = "lammps";
     repo = "lammps";
     rev = "stable_${finalAttrs.version}";
-    hash = "sha256-jx0hkiYxQlnE2sa4WTvluEgphF//sNbK91VGAQJMwjw=";
+    hash = "sha256-B2oMs9bVYO+G3yL1DGJVK/INIfANMDREV7AtC4kH3H8=";
   };
   preConfigure = ''
     cd cmake
@@ -74,11 +77,12 @@ stdenv.mkDerivation (finalAttrs: {
     inherit extraCmakeFlags;
     inherit extraBuildInputs;
   };
-  cmakeFlags = [
-  ]
-  ++ (builtins.map (p: "-DPKG_${p}=ON") (builtins.attrNames (lib.filterAttrs (n: v: v) packages)))
-  ++ (lib.mapAttrsToList (n: v: "-D${n}=${v}") extraCmakeFlags)
-  ;
+  cmakeFlags =
+    [
+      (lib.cmakeBool "BUILD_SHARED_LIBS" true)
+    ]
+    ++ (lib.mapAttrsToList (n: v: lib.cmakeBool "PKG_${n}" v) packages)
+    ++ (lib.mapAttrsToList (n: v: "-D${n}=${v}") extraCmakeFlags);
 
   buildInputs = [
     fftw
@@ -86,8 +90,7 @@ stdenv.mkDerivation (finalAttrs: {
     blas
     lapack
     gzip
-  ] ++ extraBuildInputs
-  ;
+  ] ++ lib.optionals packages.PYTHON [ python3 ] ++ extraBuildInputs;
 
   postInstall = ''
     # For backwards compatibility
@@ -99,7 +102,7 @@ stdenv.mkDerivation (finalAttrs: {
     ln -s $out/share/vim-plugins/lammps $out/share/nvim/site
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Classical Molecular Dynamics simulation code";
     longDescription = ''
       LAMMPS is a classical molecular dynamics simulation code designed to
@@ -107,15 +110,18 @@ stdenv.mkDerivation (finalAttrs: {
       National Laboratories, a US Department of Energy facility, with
       funding from the DOE. It is an open-source code, distributed freely
       under the terms of the GNU Public License (GPL).
-      '';
+    '';
     homepage = "https://www.lammps.org";
-    license = licenses.gpl2Only;
-    platforms = platforms.linux;
+    license = lib.licenses.gpl2Only;
+    platforms = lib.platforms.linux;
     # compiling lammps with 64 bit support blas and lapack might cause runtime
     # segfaults. In anycase both blas and lapack should have the same #bits
     # support.
     broken = (blas.isILP64 && lapack.isILP64);
-    maintainers = [ maintainers.costrouc maintainers.doronbehar ];
+    maintainers = with lib.maintainers; [
+      costrouc
+      doronbehar
+    ];
     mainProgram = "lmp";
   };
 })
