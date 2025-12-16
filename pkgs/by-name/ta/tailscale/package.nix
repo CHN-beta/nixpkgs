e3515a692077677d9db837b90b5b70bcc1b70fea
Haonan Chen <chn@chn.moe>
2025-12-16 17:46:25 +0800

    Revert "tailscale: fix test suite on aarch64 linux"
    This reverts commit 66c52a825a077ea7339ea1edde9b9abd68e8a2b1.

--- pkgs/by-name/ta/tailscale/package.nix
+++ pkgs/by-name/ta/tailscale/package.nix
@@ -153,10 +153,6 @@ buildGoModule (finalAttrs: {
         # Fails because we vendor dependencies
         "TestLicenseHeaders"
       ]
-      ++ lib.optionals (stdenv.hostPlatform.isLinux && stdenv.hostPlatform.isAarch64) [
-        "TestXDP"
-        "TestRTT"
-      ]
       ++ lib.optionals stdenv.hostPlatform.isDarwin [
         # syscall default route interface en0 differs from netstat
         "TestLikelyHomeRouterIPSyscallExec" # net/netmon
