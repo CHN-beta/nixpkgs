0906543909db1891554d4d6a970689ee1d30b8b3
Tom Hunze <dev@thunze.de>
2026-02-22 13:30:10 +0100

    highfive: 2.10.1 -> 3.3.0
    Changelog: https://github.com/highfive-devs/highfive/blob/v3.3.0/CHANGELOG.md
    Diff: https://github.com/highfive-devs/highfive/compare/v2.10.1...v3.3.0

--- pkgs/by-name/hi/highfive/package.nix
+++ pkgs/by-name/hi/highfive/package.nix
@@ -14,13 +14,13 @@ assert mpiSupport -> mpi != null;
 
 stdenv.mkDerivation rec {
   pname = "highfive${lib.optionalString mpiSupport "-mpi"}";
-  version = "2.10.1";
+  version = "3.3.0";
 
   src = fetchFromGitHub {
-    owner = "BlueBrain";
-    repo = "HighFive";
-    rev = "v${version}";
-    sha256 = "sha256-Nv+nbel/xGlGTB8sKF0EM1xwz/ZEri5uGB7ma6Ba6fo=";
+    owner = "highfive-devs";
+    repo = "highfive";
+    tag = "v${version}";
+    sha256 = "sha256-BuDvoQgMdZIDHYwXqigM78DQ+WtT+K0FdXERMUjmXc0=";
   };
 
   nativeBuildInputs = [ cmake ];
@@ -40,15 +40,14 @@ stdenv.mkDerivation rec {
     "-DHIGHFIVE_USE_EIGEN=ON"
     "-DHIGHFIVE_EXAMPLES=OFF"
     "-DHIGHFIVE_UNIT_TESTS=OFF"
-    "-DHIGHFIVE_USE_INSTALL_DEPS=ON"
-    (lib.cmakeFeature "CMAKE_POLICY_VERSION_MINIMUM" "3.5")
   ]
-  ++ (lib.optionals mpiSupport [ "-DHIGHFIVE_PARALLEL_HDF5=ON" ]);
+  ++ (lib.optionals mpiSupport [ "-DHDF5_IS_PARALLEL=ON" ]);
 
   meta = {
     description = "Header-only C++ HDF5 interface";
-    license = lib.licenses.boost;
-    homepage = "https://bluebrain.github.io/HighFive/";
+    homepage = "https://github.com/highfive-devs/highfive";
+    changelog = "https://github.com/highfive-devs/highfive/blob/${src.tag}/CHANGELOG.md";
+    # license = lib.licenses.boost;
     platforms = lib.platforms.unix;
     maintainers = with lib.maintainers; [ robertodr ];
   };
