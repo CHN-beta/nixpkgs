241495126d83d7236372181d8f657606824910a7
Haonan Chen <chn@chn.moe>
2026-02-25 14:31:53 +0800

    vaultwarden: 1.35.2 -> 1.35.3
    Diff: https://github.com/dani-garcia/vaultwarden/compare/1.35.2...1.35.3
    
    Changelog: https://github.com/dani-garcia/vaultwarden/releases/tag/1.35.3
    (cherry picked from commit 4900a864119892190bf32c80d8ea60752947533e)

--- pkgs/by-name/va/vaultwarden/package.nix
+++ pkgs/by-name/va/vaultwarden/package.nix
@@ -19,16 +19,16 @@ in
 
 rustPlatform.buildRustPackage rec {
   pname = "vaultwarden";
-  version = "1.34.3";
+  version = "1.35.3";
 
   src = fetchFromGitHub {
     owner = "dani-garcia";
     repo = "vaultwarden";
-    rev = version;
-    hash = "sha256-Dj0ySVRvBZ/57+UHas3VI8bi/0JBRqn0IW1Dq+405J0=";
+    tag = version;
+    hash = "sha256-Q/kMdrKMBB5Tbfi4Cy9ucI6iH8eOr+fzi876+ef2kV4=";
   };
 
-  cargoHash = "sha256-4sDagd2XGamBz1XvDj4ycRVJ0F+4iwHOPlj/RglNDqE=";
+  cargoHash = "sha256-3wS8LoFMYhd8uwjDyNfWLMQEK3USotTzTHEJNimG3pM=";
 
   # used for "Server Installed" version in admin panel
   env.VW_VERSION = version;
